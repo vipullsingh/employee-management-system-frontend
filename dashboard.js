6b7d32caf3941aadd5a595719d4ab48eb63bad3f
vipullsingh <vipul.kr.vks@gmail.com>
2023-07-13 15:41:01 +0530

    frontend fix

--- dashboard.js
+++ dashboard.js
@@ -1,56 +1,63 @@
-
-let token = localStorage.getItem('token')
+let token = localStorage.getItem('token');
 if (token == null) {
-    alert('Login First')
-    window.location.href = './index.html'
+  alert('Login First');
+  window.location.href = './index.html';
 }
 
-const logoutbtn = document.getElementById('signoutbtn')
+const logoutbtn = document.getElementById('signoutbtn');
 logoutbtn.addEventListener('click', () => {
-    localStorage.removeItem('token')
-    window.location.href = './index.html'
-})
+  localStorage.removeItem('token');
+  window.location.href = './index.html';
+});
+
+
+
 // Constants for API endpoints
-const BASE_URL = 'https://employee-mangement-system.onrender.com'; 
+const BASE_URL = 'https://employee-mangement-system.onrender.com';
 const EMPLOYEE_API = `${BASE_URL}/api/employees`;
 
-// Variables for pagination
+// Variables for pagination and filtering
 let currentPage = 1;
 const limit = 5;
+let currentFilter = '';
+let currentSearch = '';
+let currentSort = 'ascending';
 
 // Function to fetch employees for a specific page from the server
 async function getEmployeesByPage(page) {
-  try {
-    const response = await fetch(`${EMPLOYEE_API}?page=${page}&limit=${limit}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error getting employees:', error);
-    throw error;
+    try {
+      const response = await fetch(
+        `${EMPLOYEE_API}?page=${page}&limit=${limit}&department=${currentFilter}&search=${currentSearch}&sortBy=salary&sortOrder=${currentSort}`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error getting employees:', error);
+      throw error;
+    }
   }
-}
+  
 
 // Function to update pagination buttons based on previous and next links
 function updatePaginationButtons(previous, next) {
-    const previousButton = document.getElementById('previousButton');
-    const nextButton = document.getElementById('nextButton');
-  
-    previousButton.disabled = !previous;
-    nextButton.disabled = !next;
-  
-    if (previous) {
-      previousButton.addEventListener('click', () => handlePaginationClick(previous.page));
-    } else {
-      previousButton.removeEventListener('click', handlePaginationClick);
-    }
-  
-    if (next) {
-      nextButton.addEventListener('click', () => handlePaginationClick(next.page));
-    } else {
-      nextButton.removeEventListener('click', handlePaginationClick);
-    }
+  const previousButton = document.getElementById('previousButton');
+  const nextButton = document.getElementById('nextButton');
+
+  previousButton.disabled = !previous;
+  nextButton.disabled = !next;
+
+  if (previous) {
+    previousButton.addEventListener('click', () => handlePaginationClick(previous.page));
+  } else {
+    previousButton.removeEventListener('click', handlePaginationClick);
   }
-  
+
+  if (next) {
+    nextButton.addEventListener('click', () => handlePaginationClick(next.page));
+  } else {
+    nextButton.removeEventListener('click', handlePaginationClick);
+  }
+}
 
 // Function to handle pagination button click
 function handlePaginationClick(page) {
@@ -90,6 +97,7 @@ async function addEmployee(employeeData) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(employeeData),
     });
@@ -97,7 +105,7 @@ async function addEmployee(employeeData) {
     return data.employee;
   } catch (error) {
     console.error('Error adding employee:', error);
-    alert(error)
+    alert(error);
     throw error;
   }
 }
@@ -107,9 +115,12 @@ async function deleteEmployee(employeeId) {
   try {
     const response = await fetch(`${EMPLOYEE_API}/${employeeId}`, {
       method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
     const data = await response.json();
-    alert(data.message)
+    alert(data.message);
     return data.message;
   } catch (error) {
     console.error('Error deleting employee:', error);
@@ -145,33 +156,32 @@ function populateEmployeeTable(employees) {
 
 // Function to handle form submission
 async function handleFormSubmit(event) {
-    event.preventDefault();
-  
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const department = document.getElementById('department').value;
-    const salary = document.getElementById('salary').value;
-  
-    const employeeData = {
-      firstName,
-      lastName,
-      email,
-      department,
-      salary,
-    };
-  
-    try {
-      const newEmployee = await addEmployee(employeeData);
-      const employees = await getAllEmployees();
-      populateEmployeeTable(employees);
-      resetForm();
-      loadEmployees(); // Load employees again after adding a new employee
-    } catch (error) {
-      console.error('Error handling form submission:', error);
-    }
+  event.preventDefault();
+
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const email = document.getElementById('email').value;
+  const department = document.getElementById('department').value;
+  const salary = document.getElementById('salary').value;
+
+  const employeeData = {
+    firstName,
+    lastName,
+    email,
+    department,
+    salary,
+  };
+
+  try {
+    const newEmployee = await addEmployee(employeeData);
+    const employees = await getAllEmployees();
+    populateEmployeeTable(employees);
+    resetForm();
+    loadEmployees(); // Load employees again after adding a new employee
+  } catch (error) {
+    console.error('Error handling form submission:', error);
   }
-  
+}
 
 // Function to reset the form after submission
 function resetForm() {
@@ -184,60 +194,95 @@ function resetForm() {
 
 // Function to attach event listeners for delete buttons
 function attachDeleteEventListeners() {
-    const deleteButtons = document.getElementsByClassName('deleteButton');
-    Array.from(deleteButtons).forEach((button) => {
-      button.addEventListener('click', handleDelete);
-    });
-  }
+  const deleteButtons = document.getElementsByClassName('deleteButton');
+  Array.from(deleteButtons).forEach((button) => {
+    button.addEventListener('click', handleDelete);
+  });
+}
 
-  // Function to attach event listeners for edit buttons
+// Function to attach event listeners for edit buttons
 function attachEditEventListeners() {
-    const editButtons = document.getElementsByClassName('editButton');
-    Array.from(editButtons).forEach((button) => {
-      button.addEventListener('click', handleEdit);
-    });
-  }
-  
-  // Function to handle edit button click
-  function handleEdit(event) {
-    const employeeId = event.target.dataset.id;
-    window.location.href = `./employee.html?id=${employeeId}`;
-  }
+  const editButtons = document.getElementsByClassName('editButton');
+  Array.from(editButtons).forEach((button) => {
+    button.addEventListener('click', handleEdit);
+  });
+}
+
+// Function to handle edit button click
+function handleEdit(event) {
+  const employeeId = event.target.dataset.id;
+  window.location.href = `./employee.html?id=${employeeId}`;
+}
 
 // Function to handle delete button click
 async function handleDelete(event) {
-    const employeeId = event.target.dataset.id;
-  
-    try {
-      const message = await deleteEmployee(employeeId);
-      const employees = await getAllEmployees();
-      populateEmployeeTable(employees);
-    //   alert(message);
-      loadEmployees()
-    } catch (error) {
-      console.error('Error handling delete:', error);
-    }
-  }
+  const employeeId = event.target.dataset.id;
 
-// Function to delete an employee
-async function deleteEmployee(employeeId) {
-    try {
-      const response = await fetch(`${EMPLOYEE_API}/${employeeId}`, {
-        method: 'DELETE',
-      });
-      const message = await response.json();
-      alert(message.message);
-      loadEmployees()
-      return message;
-    } catch (error) {
-      console.error('Error deleting employee:', error);
-      throw error;
-    }
+  try {
+    await deleteEmployee(employeeId);
+    const employees = await getAllEmployees();
+    populateEmployeeTable(employees);
+    loadEmployees();
+  } catch (error) {
+    console.error('Error handling delete:', error);
   }
-  
+}
+
+// Function to handle filter and search button click
+function handleFilterSearch() {
+  const departmentFilter = document.getElementById('departmentFilter');
+  const searchInput = document.getElementById('searchInput');
+
+  currentFilter = departmentFilter.value;
+  currentSearch = searchInput.value;
+  currentPage = 1;
+  loadEmployees();
+}
+
+// Function to handle sort button click
+function handleSort() {
+  const sortBy = document.getElementById('sortBy');
+
+  currentSort = sortBy.value;
+  currentPage = 1;
+  loadEmployees();
+}
 
 // Event listener for form submission
 document.getElementById('addEmployeeForm').addEventListener('submit', handleFormSubmit);
 
+// Event listener for filter and search button click
+document.getElementById('filterSearchButton').addEventListener('click', handleFilterSearch);
+
+// Event listener for sort button click
+// document.getElementById('sortButton').addEventListener('click', handleSort);
+
 // Initial setup
 loadEmployees();
+
+// sort by salary asc 
+const sortBySalaryAscButton = document.querySelector(".sortbysalaryasc");
+const sortBySalaryDescButton = document.querySelector(".sortbysalarydesc");
+
+// sortBySalaryAscButton.addEventListener("click", async () => {
+//   try {
+//     const response = await fetch(`${EMPLOYEE_API}?sortBy=salary&sortOrder=asc`);
+//     const data = await response.json();
+//     console.log(data);
+//     displayEmployeeData(data.data);
+//   } catch (error) {
+//     console.error("Error sorting employees:", error);
+//   }
+// });
+
+// // sort by salary desc
+// sortBySalaryDescButton.addEventListener("click", async () => {
+//   try {
+//     const response = await fetch(`${EMPLOYEE_API}?sortBy=salary&sortOrder=desc`);
+//     const data = await response.json();
+//     console.log(data);
+//     displayEmployeeData(data.data);
+//   } catch (error) {
+//     console.error("Error sorting employees:", error);
+//   }
+// });
